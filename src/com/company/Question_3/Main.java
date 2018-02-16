30b0406d8d3f0b4a63b37128070075895188e5d8
AnthonyEisenback <anthonyeisenback@Interapts-MacBook-Pro-3.local>
2018-02-16 15:00:59 -0500

    Completed question 3

--- src/com/company/Question_3/Main.java
+++ src/com/company/Question_3/Main.java
@@ -16,6 +16,7 @@ public class Main {
 
         Main main = new Main();
         main.populateArrayList();
+        main.carShow();
     }
 
     private void populateArrayList() {
@@ -25,6 +26,15 @@ public class Main {
         carsList.add(new Car("Honda", "Civic", "Black"));
         carsList.add(new Car("Honda", "Insight", "Blue"));
         carsList.add(new Car("Tesla", "Model 3", "Grey"));
+    }
+
+    private void carShow() {
+        int number = 1;
+
+        for (Car car : carsList) {
+            System.out.println(car.getModel());
+        }
+
     }
 
 
